9bedd139deca6ec0c2dac927657e08c9d21ee5c0
ro_chl <romchik2k@gmail.com>
2021-11-05 23:25:18 -0400

    test

---
+++ src/main/java/com/example/assignment1/model/User.java
@@ -0,0 +1,9 @@
+package com.example.assignment1.model;
+
+public class User {
+
+
+    git remote add origin https://github.com/chlaps20/COMP-3095-Assignment-1..git
+    git branch -M main
+    git push -u origin main
+}
