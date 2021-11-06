21b79e65a7f8529abf05192329ea445a823ce9db
ro_chl <romchik2k@gmail.com>
2021-11-05 23:29:06 -0400

    cleaned

--- src/main/java/com/example/assignment1/model/User.java
+++ src/main/java/com/example/assignment1/model/User.java
@@ -3,7 +3,5 @@ package com.example.assignment1.model;
 public class User {
 
 
-    git remote add origin https://github.com/chlaps20/COMP-3095-Assignment-1..git
-    git branch -M main
-    git push -u origin main
+
 }
